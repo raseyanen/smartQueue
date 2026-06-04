87e8713deb6e7567087ca6574db9caa8456fca2c
Anton Makarov <makarov_a_v@sc.vsu.ru>
2026-06-04 17:18:45 +0300

    Update smartqueue.rb

--- smartqueue.rb
+++ smartqueue.rb
@@ -2,27 +2,31 @@ class Smartqueue < Formula
   desc "Smart Queue Management System - Docker-based Django app"
   homepage "https://github.com/raseyanen/smartqueue"
   url "https://github.com/raseyanen/smartqueue/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "af0e4297ed1d4d5882345967d90629c10ec2ace44c646dd6b0c27a1843cc03c8"  # Заменишь после создания тега
+  sha256 "ВСТАВЬ_ХЕШ"
   version "1.0.0"
 
   depends_on "docker"
   depends_on "docker-compose"
 
   def install
-    bin.install "bin/smartqueue"
-    prefix.install "docker-compose.prod.yml"
-    prefix.install ".env.example"
+    # Находим bin/smartqueue где угодно внутри архива
+    script = Dir["**/bin/smartqueue"].first
+    bin.install script if script
+    
+    compose = Dir["**/docker-compose.prod.yml"].first
+    prefix.install compose if compose
+    
+    env = Dir["**/.env.example"].first
+    prefix.install env if env
   end
 
   def caveats
     <<~EOS
-      First time setup:
-        1. Edit your environment file:
-           nano #{prefix}/.env.example
-        2. Then run:
-           smartqueue start
-
-      Config file is stored in ~/.smartqueue/
+      First, edit your environment file:
+        nano #{opt_prefix}/.env.example
+      
+      Then start the service:
+        smartqueue start
     EOS
   end
 end
